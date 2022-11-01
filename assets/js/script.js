25292dbfb2977fb00d2fe91fd351824c898488ba
Alain2020 <favouralain@yahoo.com>
2022-11-01 23:33:52 +0000

    Add code to check the anwer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons) {
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert('You clicked ${gametype}');
@@ -32,11 +32,37 @@ function runGame() {
      }
 }
 
+/**
+ * checks the answer agaist the first element in 
+ * the returned calculateCorrectAnswer array
+ */
+
 function checkAnswer() {
+
+    let userAnswer =parseInt(document.getElementById("answer-box").value);
+    let calculateCorrectAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculateCorrectAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right :D"),
+    } else {
+        alert(Awwwww ...arguments.you answered $(userAnswer). the correct answer was $(calculateCorrectAnswer[0]!);
+    }
+
+    runGame(calculateCorrectAnswer[1]);
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
      
 }
 
