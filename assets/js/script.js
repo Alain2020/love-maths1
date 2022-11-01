5ad0f851a8c521d80687e25b00e93ab4186e1fe2
Alain2020 <favouralain@yahoo.com>
2022-11-01 22:27:24 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,13 @@ function runGame() {
      // Creates two random numbers between 1 and 25
      let num1 = Math.floor(Math.random() * 25) + 1;
      let num2 = Math.floor(Math.random() * 25) + 1;
+
+     if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+     } else {
+          alert('Unknow game type: ${gameType}');
+          throw 'Unknow game type: ${gameType}. Aborting!';
+     }
 }
 
 function checkAnswer() {
@@ -41,8 +48,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtratQuestion(){
